bc6c01382e6c8e5cb3c14bedf2542e30e25b982d
charlesgillingham <cgillingham1@mac.com>
2015-09-22 14:51:11 -0500

    Initial Commit

---
+++ CAudioUnits/CAudioUnits.h
@@ -0,0 +1,19 @@
+//
+//  CAudioUnits.h
+//  CAudioUnits
+//
+//  Created by CHARLES GILLINGHAM on 9/22/15.
+//  Copyright (c) 2015 CharlesGillingham. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CAudioUnits.
+FOUNDATION_EXPORT double CAudioUnitsVersionNumber;
+
+//! Project version string for CAudioUnits.
+FOUNDATION_EXPORT const unsigned char CAudioUnitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CAudioUnits/PublicHeader.h>
+
+
